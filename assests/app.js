6ff6c7a1022777b1bb79dafa62aeae58a8286f6f
Chris Livingston <clivingston@gmail.com>
2021-08-26 16:31:49 -0700

    5 day forecast engaged, working on local storage next

--- assests/app.js
+++ assests/app.js
@@ -1,10 +1,22 @@
 let apiKey = "01269a22236d927374d59fe33d4e1c3c"
-let searchHistory = JSON.parse(window.localStorage.getItem('search-history')) || [];
+let archive = JSON.parse(window.localStorage.getItem('archive')) || [];
 let apiRoot = 'https://api.openweathermap.org'
-// let currentDate = moment().format('L');
+let currentDate = moment().format('L');
+let searchHistory = document.querySelector('#search-history')
+let cityInput = document.querySelector('#city-input').value
 
 let searchBtn = document.querySelector('#search-btn');
 
+function listPastSearch(citiesHistory) {
+  let li = document.createElement('li')
+  li.classList = 'list-group-item list-group-item-action'
+  li.textContent = cityInput
+  searchHistory.appendChild(li)
+}
+
+searchHistory.addEventListener('click', function(event) {
+  searchWeather(document.querySelector(this).text());
+})
 searchBtn.addEventListener('click', function(event) {
   event.preventDefault();
   let cityInput = document.querySelector('#city-input').value
@@ -25,15 +37,15 @@ function displayCurrentWeather(data) {
 
   img.setAttribute('src', 'https://openweathermap.org/img/w/'+data.weather[0].icon + '.png')
 
-  card.classList = 'card';
+  card.classList = 'card card-index';
   cardBody.classList = 'card-body';
   temp.classList = 'card-text'
   humidity.classList = 'card-text'
   wind.classList = 'card-text'
-  title.textContent = data.name;
-  temp.textContent = data.main.temp;
-  humidity.textContent = data.main.humidity;
-  wind.textContent = data.wind.speed;
+  title.textContent = 'City: ' + data.name + '(' + currentDate + ')';
+  temp.textContent = 'Temperature: ' + data.main.temp +'ºF';
+  humidity.textContent = 'Humidity: ' + data.main.humidity + '%';
+  wind.textContent = 'Wind Speed: ' + data.wind.speed + ' MPH';
 
   title.appendChild(img)
   card.appendChild(title);
@@ -55,7 +67,7 @@ function displayForecast(forecastData) {
       let forecastTemp = document.createElement('p');
       let forecastHumid = document.createElement('p');
       let forecastDates = document.createElement('p')
-      let formatDate = moment(forecastData.list[i].dt_txt.format('L'))
+      let formatDate = moment(forecastData.list[i].dt_txt).format('L')
 
       col.classList = 'col-md-2';
       card.classList = 'card bg-primary text-white';
@@ -80,6 +92,29 @@ function displayForecast(forecastData) {
   }
 }
 
+function displayUvIndex(uvData) {
+  let forecastBody = document.querySelector('.card-index')
+  let uviBtn = document.createElement('button');
+  let uvIndex = document.createElement('h6');
+
+  uviBtn.classList = 'btn';
+  uvIndex.classList = 'card-text';
+
+  uvIndex.textContent = 'UV Index: ';
+  uviBtn.textContent = uvData.current.uvi;
+
+  if (uvData.current.uvi < 4) {
+    uviBtn.classList = 'btn-success'
+  } else if (uvData.current.uvi < 7) {
+    uviBtn.classList = 'btn-warning'
+  } else {
+    uviBtn.classList = 'btn-danger'
+  }
+
+  uvIndex.appendChild(uviBtn);
+  forecastBody.appendChild(uvIndex)
+}
+
 function fetchForecast(data) {
  let apiUrl = `${apiRoot}/data/2.5/forecast?q=` + data.name + `&appid=` + apiKey + `&units=imperial`
  fetch(apiUrl)
@@ -88,13 +123,27 @@ function fetchForecast(data) {
  })
  .then(function (forecastData) {
    displayForecast(forecastData)
+   if(archive.indexOf(cityInput) === -1) {
+     archive.push(cityInput);
+     window.localStorage.setItem('archive', JSON.stringify(archive))
+     listPastSearch(cityInput)
+   }
    console.log(forecastData)
  })
 }
 
+function fetchUvIndex(data) {
+  let apiUrl = `${apiRoot}/data/2.5/onecall?lat=` + data.coord.lat +'&lon='+ data.coord.lon + `&appid=` + apiKey + `&units=imperial`;
+  fetch(apiUrl)
+  .then(function(res) {
+    return res.json()
+  })
+  .then(function(uvData) {
+    displayUvIndex(uvData)
+  })
+}
+
 function searchWeather(cityInput) {
-  let { lat } = cityInput;
-  let { lon } = cityInput;
   let apiUrl = `${apiRoot}/data/2.5/weather?q=` + cityInput + `&APPID=` + apiKey + `&units=imperial`
 
   fetch(apiUrl)
@@ -104,8 +153,17 @@ function searchWeather(cityInput) {
   .then(function (data) {
     displayCurrentWeather(data, data.name, data.main.temp, data.main.humidity, data.wind.speed)
     fetchForecast(data)
+    fetchUvIndex(data)
     // console.log(data)
   }).catch(function(err) {
     console.log(err)
   })
+}
+
+if(archive.length < 0) {
+  searchWeather(archive[archive.length - 1]);
+}
+
+for(var i = 0; i<archive.length; i++) {
+  listPastSearch(archive[i]);
 }
